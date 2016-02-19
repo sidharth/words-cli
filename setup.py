09de2e69f6f204f285e99a67f7cac72a3cea7152
sidharth <sidharth15.raja@gmail.com>
2016-02-19 16:22:45 +0530

    Finished setup

--- setup.py
+++ setup.py
@@ -3,12 +3,21 @@ import sys
 
 setup(
 	name='words-cli',
-	description='Learn some new words',
+	description='Learn some new words!',
+	long_description='Preparing for the SAT/GRE/GMAT? Why not put those extra seconds of free time to some good use with this awesome CLI!'
 	version='0.0.1',
 	author='Sidharth Raja',
 	author_email='sidharth15.raja@gmail.com'
 	license='MIT',
-	keywords='words vocabulary gre gmat sat',
+	keywords='words word vocabulary gre gmat sat language',
 	url='https://github.com/sidharth/words-cli',
-	
-	)
+	packages=['words-cli'],
+    install_requires=[
+        "click>=5.0",
+    ],
+    entry_points = {
+        'console_scripts': [
+        	'word = word.main:main'
+      ],
+    }
+)
