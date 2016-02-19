8014ecc3d239c9128cb11f5aeb9cf7fb27c38a92
sidharth <sidharth15.raja@gmail.com>
2016-02-19 16:01:57 +0530

    Added main metadata setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ import sys
 
 setup(
 	name='words-cli',
+	description='Learn some new words',
 	version='0.0.1',
-	description='')
+	author='Sidharth Raja',
+	author_email='sidharth15.raja@gmail.com'
+	license='MIT',
+	keywords='words vocabulary gre gmat sat',
+	url='https://github.com/sidharth/words-cli',
+	
+	)
